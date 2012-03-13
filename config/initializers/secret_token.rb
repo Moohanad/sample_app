52b1ab74e790cf3251a1ae426abdc0109026a0f6
CPSC 473 <cpsc473@cpsc473.(none)>
2012-03-13 12:09:36 -0700

    Finished layout and routes

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = 'f5489c23c2d3e8f61bb3f307625e7859fe8563da82b596240b7ff129bcc6d28e7a9db2fb9cda54f5ba856c41638547000de10002148b7a5ea369474c6b041f55'
+SampleApp::Application.config.secret_token = '91fd3f51fdaa02d8b8ac424c0d156060a2d81f850f070e96fb2737f7c611f3350bab32cdcaa862090ad132452661beb8bdac0a424d34426dce5381c242a6bb88'
